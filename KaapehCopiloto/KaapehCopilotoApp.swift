c6db93fbe7515646a08ca881200149a038f7a09d
Marco16005 <a00839451@tec.mx>
2025-10-27 20:52:59 -0600

    Initial Commit

---
+++ KaapehCopiloto/KaapehCopilotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaapehCopilotoApp.swift
+//  KaapehCopiloto
+//
+//  Created by Marco Antonio Torres Ramirez on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KaapehCopilotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
