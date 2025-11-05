0460fb7fc071423747924220dca240d8ac85d71c
Marco16005 <a00839451@tec.mx>
2025-11-05 11:41:06 -0600

    Initial Commit

---
+++ KaapehCopiloto2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KaapehCopiloto2
+//
+//  Created by Marco Antonio Torres Ramirez on 05/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KaapehCopiloto2/KaapehCopiloto2App.swift
@@ -0,0 +1,17 @@
+//
+//  KaapehCopiloto2App.swift
+//  KaapehCopiloto2
+//
+//  Created by Marco Antonio Torres Ramirez on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct KaapehCopiloto2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
